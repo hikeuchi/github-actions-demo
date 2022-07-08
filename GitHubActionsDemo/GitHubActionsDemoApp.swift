9ae722b1268e6e321d4e9dfd7f30c73e671f0062
Hiroki Ikeuchi <hikeuchi@morisawa.co.jp>
2022-07-08 09:39:37 +0900

    Initial Commit

---
+++ GitHubActionsDemo/GitHubActionsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsDemoApp.swift
+//  GitHubActionsDemo
+//
+//  Created by Hiroki Ikeuchi on 2022/07/08.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
